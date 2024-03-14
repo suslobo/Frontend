b912337fe02c99f043360cd96b5fc80383b5ecf1
suslobo <148755303+suslobo@users.noreply.github.com>
2024-03-14 12:33:05 +0100

    fullStack enrutado angular

--- 011.fullstack/fullstack-001/frontend/src/app/app.routes.ts
+++ 011.fullstack/fullstack-001/frontend/src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { BookListComponent } from './book-list/book-list.component';
+import { BookDetailComponent } from './book-detail/book-detail.component';
+import { BookFormComponent } from './book-form/book-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent
+    },
+    {
+        path: 'books',
+        component: BookListComponent
+    },
+    {
+        path: 'books/:id/detail', // ver el formulario
+        component: BookDetailComponent
+    },
+    {
+        path: 'books/create',
+        component: BookFormComponent
+    },
+    {
+        path: 'books/:id/update', // editar el formulario
+        component: BookFormComponent
+    },
+];
--- 011.fullstack/fullstack-001/frontend/src/app/navbar/navbar.component.ts
+++ 011.fullstack/fullstack-001/frontend/src/app/navbar/navbar.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive, NgbDropdownModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-
+  collapsed = true;
 }
